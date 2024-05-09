60fbdc8191ce5d84dd189079409b3e3aa4e76049
송민선 <poweron0335@gmail.com>
2024-05-09 18:48:55 +0900

    proejct main design fix

---
+++ src/main/java/com/example/projectsource/ProjectsourceApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectsource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectsourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectsourceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projectsource/ProjectsourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectsource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectsourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
